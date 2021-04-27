b0df39951b8e4bd3ca7e2dee8d57349a261cf16f
Dickpython <83084678+Dickpython@users.noreply.github.com>
2021-04-27 20:44:58 +0800

    Update 5.py

--- chapter2/5.py
+++ chapter2/5.py
@@ -1,1 +1,12 @@
 # 编写一个接受句子的程序，并计算大写字母和小写字母的数量。
+s = input("请输入句子")
+count = {"lettersH": 0,"lettersL": 0}
+for i in s:
+    if i.isupper():
+        count["lettersH"] += 1
+    elif i.islower():
+        count["lettersL"] += 1
+    else :
+        pass
+print("其中大写字母：",count["lettersH"])
+print("其中小写字母：",count["lettersL"])
