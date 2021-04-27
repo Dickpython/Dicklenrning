427280454cf0455c959336b6594650cd06b09479
Dickpython <83084678+Dickpython@users.noreply.github.com>
2021-04-27 18:37:22 +0800

    Update 1.py

--- chapter2/1.py
+++ chapter2/1.py
@@ -1,1 +1,5 @@
 # 编写一个程序，该程序接受一系列由空格分隔的单词作为输入，并在删除所有重复的单词并将其按字母数字顺序排序后打印这些单词。
+def none(line):
+    return line and line.strip()
+line_1 = set(list(filter(none,input("请随意输入字符"))))
+print(sorted(line_1))
