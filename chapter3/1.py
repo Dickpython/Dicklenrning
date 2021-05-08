5c4e6f8385c4d26cd8cfe3dfe36319ed33b7f467
steven <qianggao7@gmail.com>
2021-05-08 14:54:29 +0800

    add chapter3

---
+++ chapter3/1.py
@@ -0,0 +1,14 @@
+# 编写一个程序来计算输入中单词的频率。在按字母数字顺序对键进行排序后，应输出输出。
+# 如，输入：New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
+# 输出：
+# 2:2
+# 3.:1
+# 3?:1
+# New:1
+# Python:5
+# Read:1
+# and:1
+# between:1
+# choosing:1
+# or:2
+# to:1
