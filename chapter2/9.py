5344a520a53f120ea34ad8dc5bb232aa45767b97
Dickpython <83084678+Dickpython@users.noreply.github.com>
2021-05-08 22:41:28 +0800

    Update 9.py

--- chapter2/9.py
+++ chapter2/9.py
@@ -10,3 +10,12 @@
 
 # ​      输出应为：
 # ​      [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
+# _*_ coding: utf-8 _*_
+from operator import itemgetter
+line1 = []
+while True:
+    s = input("请输入：")
+    if not s:
+        break
+    line1.append(tuple(s.split(",")))
+print(sorted(line1,key = itemgetter(0,1,2)))
